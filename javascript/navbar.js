e7aca464088c700f191a0ac63138016086d4baff
BobObi <123968601+BoboObi@users.noreply.github.com>
2023-06-22 21:13:13 -0400

    Imported Google Maps and Externalised Javascript

---
+++ javascript/navbar.js
@@ -0,0 +1,32 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const close = document.querySelector(".close");
+const pageWrap = document.querySelector('.page-wrap');
+
+
+hamburger.addEventListener("click", function () {
+  navLinks.style.display =
+    navLinks.style.display === "none" ? "flex" : "none";
+    pageWrap.classList.add('disable-pointer');
+});
+
+close.addEventListener("click", function () {
+  navLinks.style.display = "none";
+  pageWrap.classList.remove('disable-pointer');
+});
+
+// Add an event listener for window resize
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 768) {
+    navLinks.style.display = "flex";
+    pageWrap.classList.remove('disable-pointer');
+  } else if (
+    window.innerWidth <= 768 &&
+    navLinks.style.display === "flex"
+
+  ) {
+    navLinks.style.display = "none";
+    pageWrap.classList.remove('disable-pointer');
+  }
+});
